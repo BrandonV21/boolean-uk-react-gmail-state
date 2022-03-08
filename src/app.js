7035176b7669efa43bfe1015e7aa11ff8b398488
BrandonV21 <brandon00095@gmail.com>
2022-03-08 16:52:33 +0000

    React Gmail State

--- src/app.js
+++ src/app.js
@@ -4,10 +4,30 @@ import initialEmails from './data/emails'
 
 import './styles/app.css'
 
+import { useState } from "react"
+
 function App() {
   // Use initialEmails for state
   console.log(initialEmails)
-
+  const [emails, setEmails] = useState(initialEmails)
+  const emailsList = emails.map(renderEmail => {
+    console.log('Logging:', renderEmail)
+    return <li className="email">
+    <div className="select">
+    <input
+      className="select-checkbox"
+      type="checkbox"/>
+    </div>
+    <div className="star">
+    <input
+      className="star-checkbox"
+      type="checkbox"
+    />
+    </div>
+    <div className="sender">{renderEmail.sender}</div>
+    <div className="title">{renderEmail.title}</div>
+  </li>
+  })
   return (
     <div className="app">
       <Header />
@@ -39,7 +59,7 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{emailsList}</main>
     </div>
   )
 }
